2c2fbc76d67f7900e2b29a0f23212a500f34c0b2
HongHyunKi <hk7286@naver.com>
2022-12-05 20:48:13 +0900

    (feat) 서버주소 config파일 생성

---
+++ config.js
@@ -0,0 +1,12 @@
+const BASE_URL = 'http://people-env.eba-35362bbh.ap-northeast-2.elasticbeanstalk.com:3001';
+
+export const API = {
+  SENTIMENT: `${BASE_URL}/sentiment`,
+  UPLOAD: `${BASE_URL}/upload`,
+  CHECKNUM: `${BASE_URL}/checkNum`,
+  RESET: `${BASE_URL}/Reset`,
+  REGISTER: `${BASE_URL}/register`,
+  LOGIN: `${BASE_URL}/login`,
+  OVERLAP: `${BASE_URL}/overlap`,
+  FACE: `${BASE_URL}/face`,
+};
