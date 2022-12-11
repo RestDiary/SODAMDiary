e472d05b45554abf30ae452eee41868d67e7f053
loginshin <95666408+loginshin@users.noreply.github.com>
2022-12-12 00:19:14 +0900

    (feat)캘린더 날짜 선택 시 카드 띄우기 성공
    date를 맞춰줘서 같다면 배열에 집넣는 형식

--- screens/CalenderScreen.js
+++ screens/CalenderScreen.js
@@ -1,75 +1,137 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import {StyleSheet, Button, View, Text, Dimensions, ScrollView } from 'react-native';
-import { Calendar } from "react-native-calendars";
+import { Calendar ,LocaleConfig} from "react-native-calendars";
+
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Card from './component/Card'
-
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from 'axios';
+import { API } from '../config.js'
 
 const { width: SCREEN_WIDTH ,height:SCREEN_HEIGHT} = Dimensions.get('window');
 
+
 function CalenderScreen({ navigation }) {
-  return (
+
+  const [diaryData, setDiaryData] = useState([]);
+  const [selectDay, setSelectDay] = useState([]); //선택한 캘린더
+  const [loading, setLoading] = useState(false);
+  const [reObjDiary, setReObjDiary] = useState([]); //선택한 다이어리 객체 재 리스트화 (사용)
+
+   //일기 data 요청
+   const getDiaryData = async () => {
+    setLoading(true)
+    const userId = await AsyncStorage.getItem("id");
+    try {
+      await axios({
+        method: "post",
+        url: `${API.MYDIARY}`,
+        params: {
+          id: userId, //****작성자 id
+        }
+      }, null)
+        .then(res => {
+          setDiaryData(res.data)
+        })
+        .catch(function (error) {
+          Alert.alert("❗error : bad response")
+        })
+    } catch (error) {
+      console.log(error)
+    }
+    setLoading(false)
+  }
+
+
+
+    //로그인 여부 확인 및 일기 불러오기
+    useEffect(() => {
+      getDiaryData()
+      console.log("객체 0번째 출력")
+      console.log(JSON.stringify(diaryData[0], null, 4))
+      }, [])
+
+      // 선택한 날짜에 해당하는 일기 객체만 집어넣기
+    function reObj(day){
+
+    }
+
+
+
+    //===================================================================================
+  return (<>
+    {diaryData[0] &&
   <View style={styles.container}>
     <View >
       <Calendar
-        // 처음에 보이는 달. default = 지금
-        // initialDate={'2022-11-24'}
         // 선택할 수 있는 최소 날짜, 이전 날짜는 회색으로 표시 기본= 표시 안됨
-        // 일기를 적기 시작한 날 시작
         minDate={'2022-10-10'}
         // 선택할 수 있는 최대 날짜 , 이후 날짜는 회색을 표시 기본 = 표시안됨
         // 오늘 날짜를 최대로 선정
-        maxDate={'2022-12-12'}
+        maxDate={'2022-12-31'}
+
+
         //day press에서 실행되는 핸들러 기본값 = 정의되지 않음
         onDayPress={day => {
-          console.log('selected day', day);
+          // console.log('selected day', day);
+          setSelectDay(day); //선택 한 날짜 2022-05-12 (0이 자동으로 붙어서 출력)
+          setReObjDiary([]);
+          for(let i = 0; i<diaryData.length;i++){
+            
+            //받아온 객체 날짜 데이터에 0 붙이기
+            const year = diaryData[i].year;
+            const month = ('0' + (diaryData[i].month)).slice(-2);
+            const days = ('0' + diaryData[i].day).slice(-2);
+            const reDiaryDate = year + '-' + month + '-' + days;
+            
+
+            
+            console.log(reDiaryDate,"   양옆 같은건가?   ",day.dateString)
+            if(reDiaryDate===day.dateString){
+              setReObjDiary(oldArray=>[...oldArray, diaryData[i]]);
+            } 
+            
+          }
+          console.log("들어간 데이터는 = ", reObjDiary)
+
         }}
-        // Handler which gets executed on day long press. Default = undefined
+
+
+        //길게 눌러 실행되는 처리기. 기본값 = 정의되지 않음
         onDayLongPress={day => {
-          console.log('selected day', day);
+          console.log('long selected day', day);
         }}
-        // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-        monthFormat={'yyyy MM'}
-        // Handler which gets executed when visible month changes in calendar. Default = undefined
+        // 달력 제목의 월 형식. 서식 값: http://arshaw.com/xdate/#Formatting
+        monthFormat={'yyyy-MM'}
+        // 달력에서 보이는 달이 바뀔 때 실행되는 핸들러. 기본값 = 정의되지 않음
         onMonthChange={month => {
           console.log('month changed', month);
         }}
         // 월 탐색 화살표 숨기기 기본 = 거짓
         hideArrows={true}
-        // Replace default arrows with custom ones (direction can be 'left' or 'right')
-        renderArrow={direction => <Arrow />}
-        // Do not show days of other months in month page. Default = false
+        // 월 페이지에 다른 달의 날짜를 표시하지 않습니다. 기본값 = 거짓
         hideExtraDays={true}
-        // If hideArrows = false and hideExtraDays = false do not switch month when tapping on greyed out
-        // day from another month that is visible in calendar page. Default = false
-        disableMonthChange={true}
-        // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
-        firstDay={1}
-        // Hide day names. Default = false
-        hideDayNames={true}
-        // Show week numbers to the left. Default = false
-        showWeekNumbers={true}
-        // Handler which gets executed when press arrow icon left. It receive a callback can go back month
-        onPressArrowLeft={subtractMonth => subtractMonth()}
-        // Handler which gets executed when press arrow icon right. It receive a callback can go next month
-        onPressArrowRight={addMonth => addMonth()}
-        // Disable left arrow. Default = false
-        disableArrowLeft={true}
-        // Disable right arrow. Default = false
-        disableArrowRight={true}
-        // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
-        disableAllTouchEventsForDisabledDays={true}
-        // Replace default month and year title with custom one. the function receive a date as parameter
-        renderHeader={date => {
-          /*Return JSX*/
-        }}
-        // Enable the option to swipe between months. Default = false
+        // 캘린더 페이지에서 볼 수 있는 다른 달의 날짜. 기본값 = 거짓
+        disableMonthChange={false}
+        // firstDay=1인 경우 일주일은 월요일부터 시작합니다. dayNames 및 dayNamesShort는 여전히 일요일부터 시작해야 합니다.
+        firstDay={0}
+        // 비활성화된 날에는 모든 터치 이벤트를 비활성화합니다. markDates에서 disableTouchEvent로 재정의할 수 있습니다.
+        disableAllTouchEventsForDisabledDays={false}
+        // // 기본 월 및 연도 제목을 사용자 정의로 바꿉니다. 함수는 매개변수로 날짜를 받습니다.        
+        // renderHeader={date => {
+        //   return(
+        //     <Text>date</Text>
+        //   )
+        // }}
+        // 월간 스와이프 옵션을 활성화합니다. 기본값 = 거짓       
         enableSwipeMonths={true}
-      />
+
+        
+        />
+        
     </View>
-        <Text style={styles.textStyle}>XXXX년 XX월 XX일 X요일</Text>
+        <Text style={styles.textStyle}>{selectDay.dateString}</Text>
     {/* 카드 가로 뷰 */}
           <View style={styles.cardContainer}>
             <SafeAreaView>
@@ -79,12 +141,21 @@ function CalenderScreen({ navigation }) {
                 horizontal style={styles.scrollView}  >
                   {/* 카드 버튼이벤트 */}
                   <View style={styles.notCard}></View>
-                  <Card id='1'></Card>
+                  {
+                    reObjDiary&&
+                    reObjDiary.map((my, index) => {
+                      return <Card key={index} data={reObjDiary[index]} />
+                    })
+                  }
                   <View style={styles.notCard}></View>
+                  
+
+
               </ScrollView>
             </SafeAreaView>
           </View>
     </View>
+                }</>
   );
 }
 
@@ -92,7 +163,7 @@ export default CalenderScreen;
 
 const styles = StyleSheet.create({
   container:{
-    backgroundColor:'#0d1c38',
+    // backgroundColor:'#0d1c38',
     width:SCREEN_WIDTH,
     height:SCREEN_HEIGHT,    
   },
@@ -101,7 +172,7 @@ const styles = StyleSheet.create({
     
   },
   textStyle:{
-    color:'#fff',
+    color:'#00000',
     padding:SCREEN_WIDTH/10
   },
   moon: {
@@ -126,5 +197,8 @@ const styles = StyleSheet.create({
   notCard:{
     width: SCREEN_WIDTH / 3,
     height: SCREEN_HEIGHT / 3,
+  },
+
+  calendar:{
   }
 })
